1ccebadd68f554855a96ee1c82e73578283ec74d
tlcgeyer <tamlincupido2402@gmail.com>
2023-12-19 10:56:11 +0200

    vue.slot-exercise

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!--Tasks
+  
+    1. Create a component called "Button" --DONE--
+    2. This component will have a template  --DONE--
+    3. Inside a template please include a button element, it will contain only a slot --DONE--
+    4. Import your Button component in your App.vue to create an instance  --DONE--
+    5. The instance will have Hello as a text
+    6. The second instance will have a "Click" and logo-->
+
+<ButtonComp>
+  <template #deText>
+    <p>Hello</p>
+  </template>
+
+  <template #deClick>
+    <p>Click</p>
+    <img src="../src/assets/logo.png" alt="logoo">
+  </template>
+</ButtonComp>
+
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ButtonComp from './components/ButtonComp.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ButtonComp
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: cursive;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
+
+
 </style>
---
+++ src/components/ButtonComp.vue
@@ -0,0 +1,18 @@
+
+<template>
+    <button class="btn btn-dark mx-5">
+        <slot name="deText"></slot>
+    </button>
+
+    <button class="btn btn-dark" >
+        <slot name="deClick"></slot>
+    </button>
+</template>
+
+<script>
+
+export default {
+    name : 'ButtonComp'
+}
+</script>
+
